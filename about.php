03aa343107d1bb6702a21bb318e73c195e02f4d8
ridingintand3m <ridingintand3mshop@gmail.com>
2023-01-05 16:21:53 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,228 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>This part of the website will contain more important information about Riding In Tand3m. This part of the website will contain more important information about Riding In Tand3m. This part of the website will contain more important information about Riding In Tand3m.</p>
+         <p>This part of the website will contain more important information about Riding In Tand3m. This part of the website will contain more important information about Riding In Tand3m.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">customer's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/customer-1.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 1</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/customer-2.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 2</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/customer-3.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 3</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/customer-4.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 4</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/customer-5.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 5</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/customer-6.jpg" alt="">
+         <p>The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Customer 6</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="staffs">
+
+   <h1 class="title">Our Staffs</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/staff-1.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 1</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/staff-2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 2</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/staff-3.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 3</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/staff-4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 4</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/staff-5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 5</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/staff-6.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+         <h3>Employee 6</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>menu links</h3>
+         <a href="home.php">HOME</a>
+         <a href="about.php">ABOUT</a>
+         <a href="shop.php">SHOP</a>
+         <a href="contact.php">CONTACT</a>
+      </div>
+
+      <div class="box">
+         <h3>access links</h3>
+         <a href="login.php">LOGIN</a>
+         <a href="register.php">REGISTER</a>
+         <a href="cart.php">CART</a>
+         <a href="orders.php">ORDERS</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us @</h3>
+         <p> <i class="fas fa-phone"></i> 0917-871-0310 </p>
+         <p> <i class="fas fa-phone"></i> 0917-871-0310 </p>
+         <p> <i class="fas fa-envelope"></i> ridingintand3m@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> M. Almeda, Pateros, 1621 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us @</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-tiktok"></i> tiktok </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Riding In Tand3m</span> </p>
+
+</section>     
